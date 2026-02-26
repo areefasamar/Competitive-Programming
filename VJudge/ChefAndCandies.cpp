260909fe09e5b1dcf4ea3bbeb2423987b02f8d52
Areefa Samar <samar4802098@cloud.neduet.edu.pk>
2026-02-26 14:23:55 +0500

    Fix formatting and add URL comment in ChefAndCandies.cpp

--- VJudge/ChefAndCandies.cpp
+++ VJudge/ChefAndCandies.cpp
@@ -1,3 +1,5 @@
+//URL: https://vjudge.net/problem/CodeChef-CHEFCAND
+
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
@@ -23,4 +25,5 @@ int main(){
 		cout<<count<<endl;
 	}
 	return 0;
+
 }
