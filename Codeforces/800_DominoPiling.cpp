8aefab84c6bc69972938afd13a62d1968ba31dc1
Areefa Samar <samar4802098@cloud.neduet.edu.pk>
2026-03-03 07:02:35 +0500

    Update problem statement and code for Domino Piling

---
+++ Codeforces/800_DominoPiling.cpp
@@ -0,0 +1,17 @@
+// Problem: Domino Piling
+// URL: https://codeforces.com/contest/50/problem/A
+// Rating: 800
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, n;
+    cin >> m >> n;
+
+    int result = (m * n) / 2;
+
+    cout << result << endl;
+
+    return 0;
+}
