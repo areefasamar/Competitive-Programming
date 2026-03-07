a1c30cedd3d4815638d2945c85b432bfd835d948
Areefa Samar <samar4802098@cloud.neduet.edu.pk>
2026-03-08 01:35:06 +0500

    Implement stock buying and selling profit calculation

---
+++ LeetCode/Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    vector<int> prices(n);
+    for(int i = 0; i < n; i++) {
+        cin >> prices[i];
+    }
+
+    int minPrice = prices[0];
+    int maxProfit = 0;
+
+    for(int i = 1; i < n; i++) {
+        if(prices[i] < minPrice)
+            minPrice = prices[i];
+        else {
+            int profit = prices[i] - minPrice;
+            if(profit > maxProfit)
+                maxProfit = profit;
+        }
+    }
+
+    cout << maxProfit << endl;
+
+    return 0;
+}
