b1619896ab8f30502601939222130c28e5dfc679
Areefa Samar <samar4802098@cloud.neduet.edu.pk>
2026-03-18 08:27:15 +0500

    Update URL comment and add calculation logic

---
+++ VJudge/Calc.cpp
@@ -0,0 +1,12 @@
+//URL: https://vjudge.net/problem/AtCoder-abc172_a
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int a;
+	cin>>a;
+	int ans;
+	ans = a + ( a * a) + ( a * a * a);
+	cout<<ans<<endl;
+	return 0;
+}
