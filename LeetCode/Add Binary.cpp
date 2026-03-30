adf50bcf74b8b984b8903329d0c71a472dc47eb2
Areefa Samar <samar4802098@cloud.neduet.edu.pk>
2026-03-30 23:31:28 +0500

    Implement binary addition in Add Binary.cpp

---
+++ LeetCode/Add Binary.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string a, b;
+    cin >> a >> b;
+
+    string result = "";
+    int i = a.size() - 1;
+    int j = b.size() - 1;
+    int carry = 0;
+
+    while(i >= 0 || j >= 0 || carry) {
+        int sum = carry;
+
+        if(i >= 0) sum += a[i--] - '0';
+        if(j >= 0) sum += b[j--] - '0';
+
+        result = char(sum % 2 + '0') + result;
+        carry = sum / 2;
+    }
+
+    cout << result;
+
+    return 0;
+}
