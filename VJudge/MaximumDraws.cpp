7b267dd438ce4da17b251e81763e201c1ae5cfa8
Areefa Samar <samar4802098@cloud.neduet.edu.pk>
2026-04-11 22:40:11 +0500

    Add problem URL comment to MaximumDraws.cpp
    Added URL comment for problem reference.

---
+++ VJudge/MaximumDraws.cpp
@@ -0,0 +1,14 @@
+//URL: https://vjudge.net/problem/HackerRank-maximum-draws
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t, n, r;
+	cin>>t;
+	while(t--){
+		cin>>n;
+		r=n+1;
+		cout<<r<<endl;
+	}
+	return 0;
+}
