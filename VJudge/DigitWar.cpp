857ca8c845b22d08a2440902b16a1cecaa4cf69a
Areefa Samar <samar4802098@cloud.neduet.edu.pk>
2026-04-19 02:03:06 +0500

    Add URL comment and implement DigitWar logic

---
+++ VJudge/DigitWar.cpp
@@ -0,0 +1,14 @@
+//URL: https://vjudge.net/contest/806028#problem/A
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int num;
+	cin>>num;
+	if(num%10==0){
+		cout<<"Rep"<<endl;
+	}
+	else{
+		cout<<"Neb"<<endl;
+	}
+	return 0;
+}
