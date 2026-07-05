509400f2d6ddd7ecabf71c3c0ff80c5f1927cf5c
Areefa Samar <samarareefa@gmail.com>
2026-07-05 12:50:28 +0500

    Add files via upload

---
+++ VJudge/ConcertTickets.cpp
@@ -0,0 +1,34 @@
+//URL: https://vjudge.net/contest/828119#problem/A
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    
+    int n, m;
+    cin >> n >> m;
+
+    multiset<int> tickets;
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        tickets.insert(x);
+    }
+
+    while (m--) {
+        int x;
+        cin >> x;
+
+        auto it = tickets.upper_bound(x);
+
+        if (it == tickets.begin()) {
+            cout << -1 << '\n';
+        } else {
+            --it;
+            cout << *it << '\n';
+            tickets.erase(it);
+        }
+    }
+
+    return 0;
+}
