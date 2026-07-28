3426e80cb981d2d48e753db96ba15c6ec4de04c9
Areefa Samar <samarareefa@gmail.com>
2026-07-28 14:30:00 +0500

    Add LowestBit.cpp

---
+++ VJudge/LowestBit.cpp
@@ -0,0 +1,13 @@
+//URL: https://vjudge.net/problem/EOlymp-1753
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int A;
+    while (cin >> A && A != 0) {
+        cout << (A & -A) << endl;
+    }
+    return 0;
+} 
+
