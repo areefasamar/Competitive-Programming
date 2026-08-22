856607c2fb2bc51319caf0595fed29d2271208f4
Areefa Samar <samar4802098@cloud.neduet.edu.pk>
2026-08-23 01:59:28 +0500

    Create Implement Stack using Queues.cpp

---
+++ LeetCode/Implement Stack using Queues.cpp
@@ -0,0 +1,34 @@
+#include <queue>
+
+class MyStack {
+private:
+    std::queue<int> q;
+
+public:
+    MyStack() {}
+    
+    void push(int x) {
+        q.push(x);
+        int sz = q.size();
+        
+        while (sz > 1) {
+            q.push(q.front());
+            q.pop();
+            sz--;
+        }
+    }
+    
+    int pop() {
+        int val = q.front();
+        q.pop();
+        return val;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        return q.empty();
+    }
+};
